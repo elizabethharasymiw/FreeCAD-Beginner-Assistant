bf68b0d1ceba4a3173b36e8d7865024b58f4569c
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-06-13 11:53:23 -0500

    Readd path_to_mod var with import call in workbench init function

--- InitGui.py
+++ InitGui.py
@@ -19,13 +19,16 @@ import os
 import FreeCAD # noqa
 import FreeCADGui # noqa
 
+from config import path_to_mod
+
 class UselessWorkbench(FreeCADGui.Workbench):
     """Purpose and functionality of the UselessWorkbench."""
 
     def __init__(self):
         self.__class__.MenuText = "FreeCAD Beginner Assistant"
         self.__class__.ToolTip = "A description of the FreeCAD Beginner Assistant"
-        self.__class__.Icon = os.path.join('.', 'icons', 'owl-2.png')
+        from config import path_to_mod
+        self.__class__.Icon = os.path.join(path_to_mod,'icons', 'owl-2.png')
 
     def Initialize(self):
         """This function is executed when the workbench is first activated.
--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 import os 
 
 import FreeCAD
+
+path_to_mod = os.path.dirname(os.path.abspath(__file__))
